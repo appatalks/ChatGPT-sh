156e9fff465e1cac9c89620efe6eb9be5b7589e6
Steven <servingbaby@users.noreply.github.com>
2023-01-13 21:07:31 -0600

    Update chatgpt.sh

--- chatgpt.sh
+++ chatgpt.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+
+# Grab prompt from input
 prompt="$1"
 
-response=$(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer API_KEY" -d "{\"prompt\":\"$prompt\", \"stop\":\"\", \"temperature\": 0.7, \"max_tokens\": 20, \"top_p\":1, \"frequency_penalty\":0,\"presence_penalty\":0 }" https://api.openai.com/v1/engines/text-davinci-002/completions)
+# Make API call
+response=$(curl -s -X POST -H "Content-Type: application/json" -H "Authorization: Bearer API_KEY" -d "{\"prompt\":\"$prompt\", \"temperature\": 0.7, \"max_tokens\": 260, \"top_p\":1, \"frequency_penalty\":0,\"presence_penalty\":0 }" https://api.openai.com/v1/engines/text-davinci-002/completions)
 
-echo $(echo $response | jq -r '.choices[0].text')
+# Extract text from response
+text=$(echo $response | jq -r '.choices[0].text')
+
+# Speak response
+# espeak -ven-us+f4 -s170 "$text"
+# flite -voice slt --setf duration_stretch=1.15 --setf int_f0_target_mean=160 -pw -t "$text"
+echo "$text"
