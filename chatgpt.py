cec7eb2ae696b398233747d577ef1b257509a1ec
Steven <servingbaby@users.noreply.github.com>
2023-01-16 10:36:57 -0600

    Adding Logging

--- chatgpt.py
+++ chatgpt.py
@@ -1,11 +1,18 @@
 import openai
 import subprocess
+from loguru import logger
+
+# Remove the default logger
+logger.remove()
+
+# Set up loguru logger 
+logger.add("/tmp/ai.log", format="{message}")
 
 # Variables
 openai.api_key = "API_KEY"
 
 # Prompt the user for their initial input
-initial_prompt = input("You: ")
+initial_prompt = input("You: ") 
 prompt = initial_prompt
 
 # Generate a response
@@ -16,7 +23,7 @@ while True:
         prompt=prompt,
         temperature=0.7,
         max_tokens=160,
-        n=2,
+        n=1,
         stop=None,
         top_p=1,
         frequency_penalty=0,
@@ -28,8 +35,11 @@ while True:
 
     # Speak the text using flite
     subprocess.run(["flite", "-voice", "slt", "--setf", "duration_stretch=1.15", "--setf", "int_f0_target_mean=160", "-pw", "-t", text])
-    
+
+    # Log the conversation
+    logger.info("User: {}", initial_prompt)
+    logger.info("AI: {}", text)
     # Concatenate the prompt with the previous question and response
-    # Still a little bit finiky compared to the website... Still needs work in response handling.
     user_input = input("You: ")
     prompt = user_input + " " + text
+    initial_prompt=user_input
